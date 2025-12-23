53042feef3b284c01ad07d6a8d6f7239c68fac53
SakharovAA <ailfulgens@gmail.com>
2025-12-23 23:48:03 +0300

    11

--- pelm_ceh/pelm_ceh.py
+++ pelm_ceh/pelm_ceh.py
@@ -29,7 +29,7 @@ Pline_farsh = (100 - a) * Pline / 100
                               #Pline_farsh - производительность линии подготовки фарша
 
 n_cutter = Pline_farsh / pcut #pcut - производительность куттера
-                              #n_cutter - количество куттерoв
+                              #n_cutter - количество куттерoвв
 
 #Вывод результатов
 print('Количество пельменных автоматов -', math.ceil(n_pelm_avt))
